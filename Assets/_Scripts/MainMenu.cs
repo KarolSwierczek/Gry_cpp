bbe9bc922da2198beed267c40c70fd2ab4ce2e17
manka.krzysztof123 <manka.krzysztof123@gmail.com>
2020-05-11 18:22:00 +0200

    MainMenu + Assets + GameDesign Environment

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject StartMenu;
+    public GameObject GameOptionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartMenu.active = true;
+        GameOptionsMenu.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void returnButtonOptionToStartMenu()
+    {
+        StartMenu.active = true;
+        GameOptionsMenu.active = false;
+    }
+
+    public void gameOptionMenuButton()
+    {
+        StartMenu.active = false;
+        GameOptionsMenu.active = true;
+    }
+}
