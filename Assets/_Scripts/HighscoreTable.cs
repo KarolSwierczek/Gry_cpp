1986ef6fa95961242965778cdb2f9a55ac0e2b61
Karol Świerczek <karolswierczek5@gmail.com>
2020-05-25 17:37:56 +0200

    fixing stuff

---
+++ Assets/_Scripts/HighscoreTable.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreTable : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        highscoreEntryList = new List<HighscoreEntry>()
+        {
+            new HighscoreEntry{ score = 23, name = "sam"},
+            new HighscoreEntry{ score = 2, name = "jak"},
+            new HighscoreEntry{ score = 45, name = "orzeł"},
+            new HighscoreEntry{ score = 33, name = "samotny"},
+            new HighscoreEntry{ score = 14, name = "Ty wiesz kto"},
+            new HighscoreEntry{ score = 9, name = "OH"},
+            new HighscoreEntry{ score = 88, name = "WOW"},
+            new HighscoreEntry{ score = 43, name = "naprawdę mi się udało"},
+            new HighscoreEntry{ score = 22, name = "blisko"},
+            new HighscoreEntry{ score = 11, name = ":p"},
+        };
+        /*
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        highscoreEntryList = highscores.highscoreEntryList;
+        */
+        // Sort entry list by score
+        for (int i = 0; i < highscoreEntryList.Count; i++)
+        {
+            for (int j = i + 1; j < highscoreEntryList.Count; j++)
+            {
+                if (highscoreEntryList[j].score > highscoreEntryList[i].score)
+                {
+                    //Swap
+                    HighscoreEntry tmp = highscoreEntryList[i];
+                    highscoreEntryList[i] = highscoreEntryList[j];
+                    highscoreEntryList[j] = tmp;
+                }
+            }
+        }
+
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+        
+        Highscores highscores = new Highscores { highscoreEntryList = highscoreEntryList };
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetString("highscoreTable"));
+        
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 30f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int score = highscoreEntry.score;
+        entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("nameText").GetComponent<Text>().text = name;
+
+        transformList.Add(entryTransform);
+    }
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+    [System.Serializable]
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
+    }
+}
---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    /*
+    public GameObject StartMenu;
+    public GameObject GameOptionsMenu;
+
+    public GameObject RButtons;
+    public GameObject PButtons;
+
+    private List<Transform> RoundButtons = new List<Transform>();
+    private List<Transform> PlayerNumber = new List<Transform>();
+
+    private Transform tempObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartMenu.active = true;
+        GameOptionsMenu.active = false;
+
+        int children = RButtons.transform.childCount;
+        int tempNumber = 5;
+
+        for (int b = 0; b < children; b++)
+        {
+            RButtons.transform.GetChild(b).GetComponent<Button>().onClick.AddListener(() =>
+            {
+                tempObject = RButtons.transform.Find("Rounds" + (tempNumber).ToString());
+
+                tempNumber = tempNumber + 5 * (b + 1);
+
+                //GetChild(1) refers to "Graphic" GameObject in Button
+                tempObject.transform.GetChild(1).GetComponent<Image>().color = Color.red;
+            });
+            RoundButtons.Add(RButtons.transform.GetChild(b));
+
+
+            PButtons.transform.GetChild(b).GetComponent<Button>().onClick.AddListener(() =>
+            {
+                tempObject = PButtons.transform.Find("Players" + (b + 2).ToString());
+
+                //GetChild(1) refers to "Graphic" GameObject in Button
+                tempObject.transform.GetChild(1).GetComponent<Image>().color = Color.red;
+            });
+            PlayerNumber.Add(PButtons.transform.GetChild(b));
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void returnButtonOptionToStartMenu()
+    {
+        StartMenu.active = true;
+        GameOptionsMenu.active = false;
+    }
+
+    public void gameOptionMenuButton()
+    {
+        StartMenu.active = false;
+        GameOptionsMenu.active = true;
+    }
+    */
+}
+
